59765520101595e96a74079d09644dce51cc4a84
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-08 22:48:37 +0400

    refactor: change cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -30,31 +30,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  if ([
-    'localhost:3000',
-    'http://localhost:3000',
-    'cartvelgram.students.nomoredomains.work',
-    'http://cartvelgram.students.nomoredomains.work',
-    'https://cartvelgram.students.nomoredomains.work',
-  ].includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-  return true;
-});
+app.use(cors({
+  origin: 'https://cartvelgram.students.nomoredomains.work/',
+  credentials: true,
+}));
 
 app.use(requestLogger);
 
