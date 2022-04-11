7165f43178e3d22417a45db6316e2ebda4e0cf61
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-11 16:03:13 +0400

    refactor: delete route /crash-test

--- backend/app.js
+++ backend/app.js
@@ -61,12 +61,6 @@ app.use(requestLogger);
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
